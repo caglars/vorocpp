0c1a1bdcad03da36e71d5bd07a0074d85ff6daa9
caglars <csinayuc@gmail.com>
2016-03-10 19:53:26 +0200

    Starting to move from python

---
+++ CSData.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Caglar Sinayuc on 10/03/16.
+//
+
+#include "CSData.h"
---
+++ CSData.h
@@ -0,0 +1,14 @@
+//
+// Created by Caglar Sinayuc on 10/03/16.
+//
+
+#ifndef VOROCPP_CSDATA_H
+#define VOROCPP_CSDATA_H
+
+
+class CSData {
+
+};
+
+
+#endif //VOROCPP_CSDATA_H
